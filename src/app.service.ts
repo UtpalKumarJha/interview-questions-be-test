bb61a73f2eef79e6feb8b0b17eebcd96aafc16e4
Utpal Kumar Jha <utpalkumarjha@hotmail.com>
2023-11-06 22:42:13 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,13 +10,16 @@ export class AppService {
   }
 
   async getJobDescriptionPrompt() {
+    console.log("Entered service");
     const completion = await openai.chat.completions.create({
       messages: [
         { "role": "user", "content": "React job 3 years" }
       ],
       model: "gpt-3.5-turbo",
     });
+    console.log("Entered service 2", completion);
     const responseMessages = completion.choices.map(choice => choice.message.content);
+    console.log("Entered service 3", responseMessages);
     return responseMessages;
   }
 }
