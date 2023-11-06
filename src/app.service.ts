6697672838744c2dd42fb41e7a4966389575a5af
pallav-sh <139957242+pallav-sh@users.noreply.github.com>
2023-11-06 23:09:25 +0530

    cahgned model

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: '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' });
 
 @Injectable()
 export class AppService {
@@ -15,7 +15,7 @@ export class AppService {
       messages: [
         { "role": "user", "content": "React job 3 years" }
       ],
-      model: "gpt-3.5-turbo",
+      model: "gpt-4",
     });
     console.log("Entered service 2", completion);
     const responseMessages = completion.choices.map(choice => choice.message.content);
